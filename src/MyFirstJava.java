de7481da563f0832a790c315029d73b0f65efff3
canaanmcarman <canaan.m.carman@gmail.com>
2021-07-06 16:15:43 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,9 @@
+/*
+This is the first Java program that will print out a message, Hello world.
+ */
+public class MyFirstJava {
+    public static void main (String[] args)
+    {
+        System.out.println("Canaan Michael Carman");
+    }
+}
